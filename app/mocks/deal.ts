3ef70961b31381def5ee66c41d8b04922f00dde9
Richard Chou <rchou@groupon.com>
2016-02-16 17:50:15 +1100

    local deals

---
+++ app/mocks/deal.ts
@@ -0,0 +1,4 @@
+export interface Deal {
+    id: number;
+    title: string;
+}
---
+++ app/mocks/mock.deals.ts
@@ -0,0 +1,7 @@
+import {} from './hero';
+
+export var HEROES: Hero[] = [
+  { "id": 11, "title": "Mr. Nice" },
+  { "id": 12, "title": "Narco" },
+  { "id": 13, "title": "Bombasto" }
+];
