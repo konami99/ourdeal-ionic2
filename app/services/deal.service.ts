92a29a7980d2137faef58e6cfcae5017d5d8cd07
Richard Chou <rchou@groupon.com>
2016-02-16 16:06:57 +1100

    local deals

--- app/services/deal.service.ts
+++ app/services/deal.service.ts
@@ -1,2 +1,10 @@
 import {Injectable} from 'angular2/core';
+import {HEROES} from '../mocks/mock.heroes.ts'
 
+
+@Injectable()
+export class HeroService {
+    getHeroes(){
+        return Promise.resolve(HEROES);
+    }
+}
